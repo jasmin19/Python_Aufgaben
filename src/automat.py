77238f2bbe759b01074c3a46d95eb84beead20ef
jasmin19 <jasmin.wycislok@nexiles.com>
2015-03-12 14:19:32 +0100

    fixed bug

--- src/automat.py
+++ src/automat.py
@@ -1,50 +1,35 @@
 
 automat = {
-	"Pizza" : "8",
-	"Twix" : "1",
-	"Malteser" : "1",
-	"Gummibaerchen" : "2",
-	"Popcorn" : "3", 
-	"Cola" : "1", 
-	"Apfel" : "1", 
-	"Salat" : "2"
+	"pizza" : "8",
+	"twix" : "1",
+	"malteser" : "1",
+	"gummibaerchen" : "2",
+	"popcorn" : "3", 
+	"cola" : "1", 
+	"apfel" : "1", 
+	"salat" : "2"
 }
 
 inp = raw_input("Was wollen sie? ")
 
-
 for essen in automat:
-	if inp == essen:
+	if inp == essen.lower() or inp == essen.upper() or inp == essen.title():
 		print "das macht", automat[essen], "EUR."
 
-		anzahl_muenzen = 1 * automat[essen]
-		print "Werfen Sie", anzahl_muenzen, "1 Euro Muenze(n) ein"
-
-
-	anf = 0
-	einwerf = raw_input("Bitte Muenzen einwerfen: ")
-
-
-	if anf < int(automat[essen]):
-		print anf
+		anzahl_muenzen = 1 * int(automat[essen])
+		print "Werfen Sie", anzahl_muenzen, "1 Euro Muenze(n) ein."
+		break
+else: 
+	print "ERROR! Dieses Produkt gibt es nicht."
 	
+anf = 0
+while anf <= automat[essen]:
+	einwerf = input("Bitte Muenzen einwerfen: ")
 	if einwerf == 1:
-		anf= anf + 1
-		print "Noch", automat[essen] - anf, "Muenze(n) einwerfen!"
-	elif anf == automat[essen]:
-		print "Danke fuer ihren Einkauf!"
+		anf+=1
+		print "Noch", anzahl_muenzen - anf, "Muenze(n) einwerfen!"
+		if anf == anzahl_muenzen:
+			print "Danke fuer ihren Einkauf!"
+			break
 
-
-# anf = 0
-# einwerf = ' '
-# while anf < int(automat[essen]):
-# 	einwerf = raw_input("Bitte Muenzen einwerfen:")
-# 	print anf
-# 	if einwerf == 1:
-# 		anf= anf + 1
-# 		break
-
-# 		print "Noch", automat[essen] - anf, "Muenze(n) einwerfen!"
-# 	elif anf == automat[essen]:
-# 		print "Danke fuer ihren Einkauf!"
 	 
