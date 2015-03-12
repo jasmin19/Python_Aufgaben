77b2d7f9541fcc9e653fe4ce3263a1f45fdb6730
jasmin19 <jasmin.wycislok@nexiles.com>
2015-03-12 09:07:08 +0100

    added code

--- src/automat.py
+++ src/automat.py
@@ -1,3 +1,50 @@
-#thats from Jan
 
-automat = ["Pizza", "Twix", "Malteser", "Gummibärchen", "Popcorn", "Cola", "Apfel", "Salat"]
+automat = {
+	"Pizza" : "8",
+	"Twix" : "1",
+	"Malteser" : "1",
+	"Gummibaerchen" : "2",
+	"Popcorn" : "3", 
+	"Cola" : "1", 
+	"Apfel" : "1", 
+	"Salat" : "2"
+}
+
+inp = raw_input("Was wollen sie? ")
+
+
+for essen in automat:
+	if inp == essen:
+		print "das macht", automat[essen], "EUR."
+
+		anzahl_muenzen = 1 * automat[essen]
+		print "Werfen Sie", anzahl_muenzen, "1 Euro Muenze(n) ein"
+
+
+	anf = 0
+	einwerf = raw_input("Bitte Muenzen einwerfen: ")
+
+
+	if anf < int(automat[essen]):
+		print anf
+	
+	if einwerf == 1:
+		anf= anf + 1
+		print "Noch", automat[essen] - anf, "Muenze(n) einwerfen!"
+	elif anf == automat[essen]:
+		print "Danke fuer ihren Einkauf!"
+
+
+# anf = 0
+# einwerf = ' '
+# while anf < int(automat[essen]):
+# 	einwerf = raw_input("Bitte Muenzen einwerfen:")
+# 	print anf
+# 	if einwerf == 1:
+# 		anf= anf + 1
+# 		break
+
+# 		print "Noch", automat[essen] - anf, "Muenze(n) einwerfen!"
+# 	elif anf == automat[essen]:
+# 		print "Danke fuer ihren Einkauf!"
+	 
